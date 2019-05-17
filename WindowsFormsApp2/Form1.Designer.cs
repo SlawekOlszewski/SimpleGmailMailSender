7b006b563183865133bf002912001d72d02798d8
Sławomir Olszewski <slawomir.olszew@gmail.com>
2019-05-17 15:20:15 +0200

    added files

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.reciverTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.yourMail = new System.Windows.Forms.TextBox();
+            this.yourPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(60, 160);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(353, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.Location = new System.Drawing.Point(60, 200);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(353, 134);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Subject";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Message";
+            // 
+            // reciverTextBox
+            // 
+            this.reciverTextBox.Location = new System.Drawing.Point(60, 124);
+            this.reciverTextBox.Name = "reciverTextBox";
+            this.reciverTextBox.Size = new System.Drawing.Size(353, 20);
+            this.reciverTextBox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "To";
+            // 
+            // yourMail
+            // 
+            this.yourMail.Location = new System.Drawing.Point(120, 12);
+            this.yourMail.Name = "yourMail";
+            this.yourMail.Size = new System.Drawing.Size(177, 20);
+            this.yourMail.TabIndex = 11;
+            // 
+            // yourPassword
+            // 
+            this.yourPassword.Location = new System.Drawing.Point(120, 58);
+            this.yourPassword.Name = "yourPassword";
+            this.yourPassword.Size = new System.Drawing.Size(177, 20);
+            this.yourPassword.TabIndex = 12;
+            this.yourPassword.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Your gmail adress";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Your password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 391);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.yourPassword);
+            this.Controls.Add(this.yourMail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.reciverTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.RichTextBox lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox reciverTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox yourMail;
+        private System.Windows.Forms.TextBox yourPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            MailMessage msg = new MailMessage();
+
+                String reciver = reciverTextBox.Text;
+                msg.To.Add(reciver);
+
+                String adress = yourMail.Text;
+                String password = yourPassword.Text;
+
+                MailAddress address = new MailAddress(adress);
+                msg.From = address;
+
+                msg.Subject = txtName.Text;
+                msg.Body = lblResult.Text;
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true; //only enable this if your provider requires it
+                                         //Setup credentials to login to our sender email address ("UserName", "Password")
+                NetworkCredential credentials = new NetworkCredential(adress, password);
+                client.Credentials = credentials;
+
+  
+                client.Send(msg);
+
+                //Display some feedback to the user to let them know it was sent
+                MessageBox.Show("Your message was sent!", "Message sent");
+
+                //Clear the form
+                reciverTextBox.Text = "";
+                yourMail.Text = "";
+                yourPassword.Text = "";
+                lblResult.Text = "";
+                txtName.Text = "";
+            }
+            catch
+            {
+                //If the message failed at some point, let the user know
+                MessageBox.Show("Your message failed to send, please try again.", "Sending error");
+            }
+        }
+    }
+}
